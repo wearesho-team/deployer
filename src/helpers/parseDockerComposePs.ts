11b0f1c0ad8c9b6c67d2cf4bbd321a27d48a8a1b
Alexander Letnikow <reclamme@gmail.com>
2019-02-13 20:18:26 +0200

    Add docker-compose ps example stdout

--- src/helpers/parseDockerComposePs.ts
+++ src/helpers/parseDockerComposePs.ts
@@ -1,5 +1,12 @@
 import { Container } from "data";
 
+/**
+ * @param stdout Example
+          Name                        Command                 State             Ports
+---------------------------------------------------------------------------------------------
+mywordpress_db_1          docker-entrypoint.sh mysqld      Up (healthy)  3306/tcp
+mywordpress_wordpress_1   /entrypoint.sh apache2-for ...   Restarting    0.0.0.0:8000->80/tcp
+ */
 export const parseDockerComposePs = (stdout: string): Array<Container> => (
     stdout.split("\n").splice(2)
         .map(containerString => containerString.split(/\s{3,}/))
